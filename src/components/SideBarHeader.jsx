eda8eea985fbffa760fea23ec4c073607f5c0272
rolandoloera <roliclock@gmail.com>
2024-04-11 11:18:31 -0600

    copiar jugador

--- src/components/SideBarHeader.jsx
+++ src/components/SideBarHeader.jsx
@@ -1,7 +1,7 @@
 import '../css/SideBarHeader.css';
 export const SideBarHeader = ({titulo='Encabezado'}) => {
   return (
-        <div className="header" hidden={true}>
+        <div className="header" hidden={false}>
           <h2 className="text">{titulo}</h2>
         </div>
   )
